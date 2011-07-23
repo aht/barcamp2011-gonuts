54520c9e91a1a86d007f34253e76871b4a1bea18
Trinh Hai-Anh <trinh.haianh@skunkworks.vn>
2011-07-23 22:29:04 +0700

    echosrv not to die on client error, which get logged

--- echosrv.go
+++ echosrv.go
@@ -7,13 +7,16 @@ func die(err os.Error) {
 }
 
 func main() {
-    listener, err := net.Listen("tcp", "127.0.0.1:3640"); die(err)
+    server, err := net.Listen("tcp", "127.0.0.1:3640"); die(err)
     for {
-        conn, err := listener.Accept(); die(err)
+        conn, err := server.Accept(); die(err)
         go func() {
             defer conn.Close()
-            n, err := io.Copy(conn, conn); die(err)
-            log.Printf("echoed %d byte from %s\n", n, conn.RemoteAddr())
+            n, err := io.Copy(conn, conn)
+            log.Printf("echoed %d byte to %s\n", n, conn.RemoteAddr())
+            if err != nil {
+                log.Printf("error: %s", err)
+            }
         }()
     }
 }
